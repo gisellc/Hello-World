90df68de7a8a2ca5fa79d02cd6edbe5182c5fe40
Giselle Colon <giselle.colon@okstate.edu>
2017-05-10 15:32:04 -0500

    rc branch

--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,6 @@ import java.lang.System;
 
 public class helloWorld{
    public static void main(String[] args){
-      System.out.println("Hello Universe");
-      System.out.println("Hello You");
+      System.out.println("rc branch");
    }
 }
